ed90a358a6b27169c62c5af67ff2896ea7f97269
Aleksander Morozov <alex.morozov.dd@mail.ru>
2022-11-22 22:59:47 +0300

    All basic calculations are working

--- include/calc.h
+++ include/calc.h
@@ -3,11 +3,25 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
 #include "fileUtils.h"
 
 const int MAX_DEF_SIZE = 1000;
 
 char* GetInputLine ();
+
+int GetG ();
+
+int GetN ();
+
+int GetE ();
+
+int GetT ();
+
+int GetP ();
+
+void SkipSpaces ();
 
 #endif
--- src/calc.cpp
+++ src/calc.cpp
@@ -1,20 +1,132 @@
 #include "calc.h"
 
+// The implementation is far from being the best of the best 
+// because the main point of this repo is to learn the new algorithm
+
+const char* string = NULL;
+
 int main()
 {
-    char* string = GetInputLine ();
+    string = GetInputLine();
 
-    GetN (&string);
+    int res = GetG();
 
-    printf ("Got string %s\n", string);
+    printf ("Got result %d\n", res);
 }
 
 
-int GetN (char** string)
+int GetG () 
 {
+    SkipSpaces();
+
+    int val = GetE();
+
+    if (*string == '\0')
+    {
+        printf ("G: Got termination symbol, ending\n");
+        return val;
+    }
+    else
+    {
+        printf ("Compilation error at char %c\n", *string);
+    }
+
+
+    return val;
+}
+
+
+int GetE ()
+{
+    SkipSpaces();
+
+    int val = GetT ();
+
+    while (*string == '+' || *string == '-')
+    {
+        int last_op = *string;
+        string++;
+        int cur_val = GetT ();
+
+        if (last_op == '+')
+            val += cur_val;
+        else
+            val -= cur_val;
+    }   
+
+    return val;
+}
+
+
+int GetT()
+{
+    SkipSpaces();
+
+    int val = GetP ();
+
+    while (*string == '*' || *string == '/')
+    {
+        int last_op = *string;
+        string++;
+        int cur_val = GetP ();
+
+        if (last_op == '*')
+            val *= cur_val;
+        else
+            val /= cur_val;
+    }   
+
+    printf ("T: Passing up %c\n", *string);
+
+    return val;
+}
+
+
+int GetP ()
+{
+    SkipSpaces();
+
+    int val = 0;
+
+    if (*string == '(')
+    {
+        string++;
+
+        val = GetE ();
+        if (*string != ')') printf ("Missing \')\' - end of subexpression\n");
+        
+        string++;
+
+        SkipSpaces ();
+    }
+    else
+    {
+        val = GetN ();
+    }
+
+    return val;
+}
+
+
+int GetN ()
+{
+    SkipSpaces();
+
     int val = 0;
     
-    const char* OldString = *string;
+    const char* OldString = string;
+
+    while ('0' <= *string && *string <= '9')
+    {
+        val = val*10 + (*string - '0');
+        string++;
+
+        SkipSpaces();
+    }
+
+    printf ("N: Passing up %c\n", *string);
+
+    return val;
 }
 
 
@@ -30,4 +142,10 @@ char* GetInputLine ()
     fclose (input_file);
 
     return buffer;   
+}
+
+
+void SkipSpaces ()
+{
+    while (isspace (*string)) string++;
 }
